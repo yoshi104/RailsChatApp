3841342c25bbfad8b47144d561eed055ddfa5a34
yoshi104 <yoshiyukikt@gmail.com>
2018-11-21 00:28:47 +0900

    chatコントローラを実装しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get '' => 'top/layouts#index'
-  get 'detail' => 'detailpage#index'
-  get 'top' => redirect('')
-  get 'index' => redirect('')
 
-  get 'top/index'
+ resources :chats
+ root to: 'chats#index'
+ get 'chats' => redirect('')
+ get 'index' => redirect('')
+ 
+ # get 'chats' => redirect('')
+ # get '' => 'top/layouts#chatView'
+ # get 'detail' => 'detailpage#chatView'
+ # get 'top' => redirect('')
+ # get 'index' => redirect('')
+ # get 'layouts/chatView'
+ # root to: 'layouts#chatView'
 
 end
